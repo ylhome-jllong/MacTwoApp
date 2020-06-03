d9160df0e2d491caac0846583213ac620719eea2
江龙 <yllink@163.com>
2020-06-03 21:50:30 +0800

    Initial Commit

---
+++ MacTwoApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacTwoApp
+//
+//  Created by 江龙 on 2020/6/3.
+//  Copyright © 2020 江龙. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacTwoApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacTwoApp
+//
+//  Created by 江龙 on 2020/6/3.
+//  Copyright © 2020 江龙. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
